b42e6c1b228896fab540e34c160eee3509b46d65
RAFYA HASSAN <127591534+rafyahassan@users.noreply.github.com>
2025-02-17 20:57:39 +0000

    Update ddl_bronze_layer.sql

--- scripts/Bronze_layer/ddl_bronze_layer.sql
+++ scripts/Bronze_layer/ddl_bronze_layer.sql
@@ -1,129 +1,90 @@
-EXECUTE bronze.load_bronze
-CREATE OR ALTER PROCEDURE bronze.load_bronze as 
-BEGIN
-	DECLARE @start_time DATETIME , @end_date DATETIME;
-	DECLARE @start_bronze_time datetime, @end_bronze_time datetime;
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
 
-	
-	BEGIN TRY 
-	SET @start_bronze_time=GETDATE();
-		PRINT'==============================================';
-		PRINT'LAODING BRONZE TABLES ';
-		PRINT'==============================================';
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_cust_info;
+GO
 
-		PRINT'==============================================';
-		PRINT'-----------CRM TABLES-------------------------';
-		PRINT'==============================================';
-		PRINT'>>>>Table Cust_info';
+CREATE TABLE bronze.crm_cust_info (
+    cst_id              INT,
+    cst_key             NVARCHAR(50),
+    cst_firstname       NVARCHAR(50),
+    cst_lastname        NVARCHAR(50),
+    cst_marital_status  NVARCHAR(50),
+    cst_gndr            NVARCHAR(50),
+    cst_create_date     DATE
+);
+GO
 
-		SET @start_time=GETDATE();
-		TRUNCATE TABLE bronze.crm_cust_info
-		Bulk INSERT bronze.crm_cust_info
-		FROM 'C:\Users\Hp\Desktop\Formations\SQL-DataWerhouseProject\cust_info.csv'
-		WITH (
-			FIRSTROW=2,
-			FIELDTERMINATOR=',',
-			TABLOCK
-		);
-		SET @end_date=GETDATE();
-		PRINT'loading duration: '+CAST(DATEDIFF(second,@start_time,@end_date) AS NVARCHAR)+'secands'
-		PRINT'-------------------------------------------------';
-		SELECT count(*) FROM bronze.crm_cust_info
+IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_prd_info;
+GO
 
-		SET @start_time=GETDATE();
-		PRINT'>>>>Table prd_info';
-		TRUNCATE TABLE bronze.crm_prd_info
-		Bulk INSERT bronze.crm_prd_info
-		FROM 'C:\Users\Hp\Desktop\Formations\SQL-DataWerhouseProject\prd_info.csv'
-		WITH (
-			FIRSTROW=2,
-			FIELDTERMINATOR=',',
-			TABLOCK
-		);
-		SET @end_date=GETDATE();
-		PRINT'loading duration: '+CAST(DATEDIFF(second,@start_time,@end_date) AS NVARCHAR)+'secands'
-		PRINT'-------------------------------------------------';
-		SELECT count(*) FROM bronze.crm_prd_info
+CREATE TABLE bronze.crm_prd_info (
+    prd_id       INT,
+    prd_key      NVARCHAR(50),
+    prd_nm       NVARCHAR(50),
+    prd_cost     INT,
+    prd_line     NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt   DATETIME
+);
+GO
 
-		PRINT'>>>>Table sales_details';
-		SET @start_time=GETDATE();
-		TRUNCATE TABLE bronze.crm_sales_details
-		Bulk INSERT bronze.crm_sales_details
-		FROM 'C:\Users\Hp\Desktop\Formations\SQL-DataWerhouseProject\sales_details.csv'
-		WITH (
-			FIRSTROW=2,
-			FIELDTERMINATOR=',',
-			TABLOCK
-		);
-		SET @end_date=GETDATE();
-		PRINT'loading duration: '+CAST(DATEDIFF(second,@start_time,@end_date) AS NVARCHAR)+'secands'
-		PRINT'-------------------------------------------------';
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_sales_details;
+GO
 
-		SELECT COUNT(*) FROM bronze.crm_sales_details
+CREATE TABLE bronze.crm_sales_details (
+    sls_ord_num  NVARCHAR(50),
+    sls_prd_key  NVARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT
+);
+GO
 
-		---ERP TABLES
-		PRINT'==============================================';
-		PRINT'-----------ERP TABLES-------------------------';
-		PRINT'==============================================';
-	
-		PRINT'>>>>Table cust_az12';
-		SET @start_time=GETDATE();
-		TRUNCATE TABLE bronze.erp_cust_az12
-		Bulk INSERT bronze.erp_cust_az12
-		FROM 'C:\Users\Hp\Desktop\Formations\SQL-DataWerhouseProject\CUST_AZ12.csv'
-		WITH (
-			FIRSTROW=2,
-			FIELDTERMINATOR=',',
-			TABLOCK
-		);
-		SET @end_date=GETDATE();
-		PRINT'loading duration: '+CAST(DATEDIFF(second,@start_time,@end_date) AS NVARCHAR)+'secands'
-		PRINT'-------------------------------------------------';
+IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_loc_a101;
+GO
 
-		SELECT count(*) FROM bronze.erp_cust_az12
+CREATE TABLE bronze.erp_loc_a101 (
+    cid    NVARCHAR(50),
+    cntry  NVARCHAR(50)
+);
+GO
 
-		PRINT'>>>>Table loc_a101';
-		SET @start_time=GETDATE();
-		TRUNCATE TABLE bronze.erp_loc_a101
-		Bulk INSERT bronze.erp_loc_a101
-		FROM 'C:\Users\Hp\Desktop\Formations\SQL-DataWerhouseProject\LOC_A101.csv'
-		WITH (
-			FIRSTROW=2,
-			FIELDTERMINATOR=',',
-			TABLOCK
-		);
-		SET @end_date=GETDATE();
-		PRINT'loading duration: '+CAST(DATEDIFF(second,@start_time,@end_date) AS NVARCHAR)+'secands'
-		PRINT'-------------------------------------------------';
+IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_cust_az12;
+GO
 
-		SELECT count(*) FROM bronze.erp_loc_a101
+CREATE TABLE bronze.erp_cust_az12 (
+    cid    NVARCHAR(50),
+    bdate  DATE,
+    gen    NVARCHAR(50)
+);
+GO
 
-		PRINT'>>>>Table px_cat_g1v2';
+IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_px_cat_g1v2;
+GO
 
-		SET @start_time=GETDATE();
-		TRUNCATE TABLE bronze.erp_px_cat_g1v2
-		Bulk INSERT bronze.erp_px_cat_g1v2
-		FROM 'C:\Users\Hp\Desktop\Formations\SQL-DataWerhouseProject\PX_CAT_G1V2.csv'
-		WITH (
-			FIRSTROW=2,
-			FIELDTERMINATOR=',',
-			TABLOCK
-		);
-		SET @end_date=GETDATE();
-		PRINT'loading duration: '+CAST(DATEDIFF(second,@start_time,@end_date) AS NVARCHAR)+'secands'
-		PRINT'-------------------------------------------------';
-
-		SELECT count(*) FROM bronze.erp_px_cat_g1v2
-
-		SET @end_bronze_time=GETDATE()
-		PRINT'Completion time :'+CAST(DATEDIFF(second,@start_bronze_time,@end_bronze_time) as nvarchar)+' seconds';
-
-	END TRY 
-	BEGIN CATCH 
-	PRINT'==========================================';
-	PRINT'ERROR OCURED DURING BRONZE LAYER ';
-	PRINT'ERROR message'+ERROR_MESSAGE();
-	PRINT'ERROR message'+ cast(ERROR_NUMBER() as  nvarchar);
-	END CATCH
-
-END
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    id           NVARCHAR(50),
+    cat          NVARCHAR(50),
+    subcat       NVARCHAR(50),
+    maintenance  NVARCHAR(50)
+);
+GO
